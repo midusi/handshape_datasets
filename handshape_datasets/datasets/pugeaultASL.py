6b2d0d7af04e06d0acc1fbe6cc16ab626e19dc07
CortiPum <cortidice@gmail.com>
2020-04-07 21:21:02 -0300

    Fix the CIARP returns format. Now return metadata (clases, subjects-folder) and x (it will contain 3 folders test_camera, test_kinect, train_kinect -->7127 subject[image]=0 --> belongs to test_camera). Added CIARP labels. Implemented PugeaultASLAInfo and PugeaultASLBInfo. The methods load in PugeaultASLB is not implemented, cause of depth image, so added in requeriments OpenCV. Fix a minor bug in base -->format_size

--- handshape_datasets/datasets/pugeaultASL.py
+++ handshape_datasets/datasets/pugeaultASL.py
@@ -152,7 +152,7 @@ class PugeaultASL_B(DatasetLoader):
         #https://medium.com/@a.ydobon/python-in-depth-image-handling-in-python-with-opencv-1-be44da5db5c9
         image_path_test="C:\\Users\\corti\\.handshape_datasets"
         image=cv.imread(image_path_test[cv.IMREAD_UNCHANGED])
-
+#
 
 
         #cvLoadImage(filename.c_str(), CV_LOAD_IMAGE_UNCHANGED)
