336408b41b2e72ea93842d9be9dd24c20b6693db
JereUR <jeremias.jdv@gmail.com>
2020-04-19 18:40:03 +0200

    Format fix

--- testing/list_all.py
+++ testing/list_all.py
@@ -2,8 +2,8 @@ import handshape_datasets as hd
 import logging
 
 
-hd.list_datasets()
-#x,metadata=hd.load("Nus1")
+#hd.list_datasets()
+x,metadata=hd.load("lsa16")
 
 logging.basicConfig(format='%(levelname)s:%(message)s',level=logging.DEBUG)
 #logging.debug(f"This message should go to the log file")
